d68f165bcaa55bccb06dd6e2389da26cb0102b92
Zariah <zariahhaynie@gmail.com>
2026-09-21 09:56:57 -0600

    score

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string score;  
+        Console.Write("Please enter your grade score: ");
+        score = Console.ReadLine();
+        int percentage = int.Parse(score);
+
+        if (percentage > 90)
+            Console.WriteLine("You got an A");
+        else if (percentage > 80)
+            Console.WriteLine("You got a B");
+        else if (percentage > 70)
+            Console.WriteLine("You got a C");
+         else if (percentage > 60)
+            Console.WriteLine("You got a D");
+        else
+            Console.WriteLine("You got an F");
+
     }
 }
