82f76a006a84f15dd4c1cdaabbd23ad663e5c5cf
Zariah <zariahhaynie@gmail.com>
2026-09-21 09:17:07 -0600

    names

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.Write("Please enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("Please enter your last name: ");
+        lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
+
