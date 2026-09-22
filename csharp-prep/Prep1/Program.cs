063498c20d84b15b2210e08971b38ca5ff0eb28b
Zariah <zariahhaynie@gmail.com>
2026-09-22 17:38:30 -0600

    f

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,11 +6,13 @@ class Program
     {
         string firstName;
         string lastName;
+        
         Console.Write("Please enter your first name: ");
         firstName = Console.ReadLine();
 
         Console.Write("Please enter your last name: ");
         lastName = Console.ReadLine();
+
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
